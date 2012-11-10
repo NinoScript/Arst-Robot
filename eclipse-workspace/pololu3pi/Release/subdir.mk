a156129513ffd5d9b9c5d52439b343f4336d623e
Cristián Arenas Ulloa <cristian.arenas@gmail.com>
2012-11-10 18:09:13 -0300

    comit inicial del proyecto, después devuelvo los gitignores

---
+++ eclipse-workspace/pololu3pi/Release/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../main.cc 
+
+OBJS += \
+./main.o 
+
+CC_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -Wall -Os -fpack-struct -fshort-enums -funsigned-char -funsigned-bitfields -fno-exceptions -mmcu=atmega328p -DF_CPU=20000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
