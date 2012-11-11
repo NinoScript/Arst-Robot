813013c302aa979c9c02cc8050d975052a0daa74
Cristián Arenas Ulloa <cristian.arenas@gmail.com>
2012-11-10 22:57:53 -0300

    Revert "Revert "clase movimiento""
    This reverts commit 874e7dfdb3c82e72736b0003a213d33ecdeb5f9a.

---
+++ eclipse-workspace/pololu3pi/Movimiento.h
@@ -0,0 +1,17 @@
+/*
+ * Movimiento.h
+ *
+ *  Created on: Nov 10, 2012
+ *      Author: ninoscript
+ */
+
+#ifndef MOVIMIENTO_H_
+#define MOVIMIENTO_H_
+
+class Movimiento {
+public:
+	Movimiento();
+	virtual ~Movimiento();
+};
+
+#endif /* MOVIMIENTO_H_ */
