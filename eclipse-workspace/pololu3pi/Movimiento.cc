429d705522a699481553e4977c64b47f81558a07
Cristián Arenas Ulloa <cristian.arenas@gmail.com>
2012-11-10 21:38:43 -0300

    clase movimiento

---
+++ eclipse-workspace/pololu3pi/Movimiento.cc
@@ -0,0 +1,18 @@
+/*
+ * Movimiento.cc
+ *
+ *  Created on: Nov 10, 2012
+ *      Author: ninoscript
+ */
+
+#include "Movimiento.h"
+
+Movimiento::Movimiento() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Movimiento::~Movimiento() {
+	// TODO Auto-generated destructor stub
+}
+
