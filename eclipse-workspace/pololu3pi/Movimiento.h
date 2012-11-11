f0c34396e89571c096767fb6ad35249f66f7506c
Cristián Arenas Ulloa <cristian.arenas@gmail.com>
2012-11-10 23:03:49 -0300

    volviendo a tener , no se wn, me desconcentró este wn

--- eclipse-workspace/pololu3pi/Movimiento.h
+++ eclipse-workspace/pololu3pi/Movimiento.h
@@ -8,10 +8,14 @@
 #ifndef MOVIMIENTO_H_
 #define MOVIMIENTO_H_
 
+#include "pololu/3pi.h"
+
 class Movimiento {
 public:
-	Movimiento();
-	virtual ~Movimiento();
+	unsigned int velmax;
+
+	void avanzarUnCuadrado ();
+	void girarDerecha ();
 };
 
 #endif /* MOVIMIENTO_H_ */
