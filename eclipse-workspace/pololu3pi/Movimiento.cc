0c613ae138e6030621b37a22aaf1dd19c5cf4f2a
luis-ojeda <luisjojeda1@gmail.com>
2012-11-13 09:46:20 -0300

    Revert "Se añaden clase point. Comienza programación de mover inteligentemente"
    This reverts commit 8c7b380f3056ca9c0560e8f425e8fcecce822286.

--- eclipse-workspace/pololu3pi/Movimiento.cc
+++ eclipse-workspace/pololu3pi/Movimiento.cc
@@ -20,6 +20,7 @@ void Movimiento::girarDer () {
 	detenerse();
 }
 
+<<<<<<< HEAD
 void Movimiento::girarIzq () {
 	OrangutanMotors::setSpeeds(-velocidad_maxima_giro,
 				   velocidad_maxima_giro);
@@ -67,3 +68,10 @@ int Movimiento::mover (point & posicion, point const & objetivo, point & direcci
 	return posicion.distancia(objetivo);
 
 }
+=======
+void Movimiento::actualizarFactores(unsigned char nueva_velocidad_maxima) {
+	velocidad_maxima = nueva_velocidad_maxima;
+	factor_velocidad = 47500 / velocidad_maxima;
+	factor_giro      = 16000 / velocidad_maxima;
+}
+>>>>>>> Revert "Se añaden clase point. Comienza programación de mover inteligentemente"
