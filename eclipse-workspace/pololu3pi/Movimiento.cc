6b7e7efba6789193851cd5bed4b7f2713ad60f38
luis-ojeda <luisjojeda1@gmail.com>
2012-11-13 10:52:26 -0300

    Revert "sadf"
    This reverts commit 4e48af9a2665ffb3024467aa0fb3f3e9b281976c.

--- eclipse-workspace/pololu3pi/Movimiento.cc
+++ eclipse-workspace/pololu3pi/Movimiento.cc
@@ -11,6 +11,7 @@ void Movimiento::avanzarUnCuadrado () {
 	OrangutanMotors::setSpeeds(velocidad_maxima_avance,
 				   velocidad_maxima_avance);
 	delay_ms(tiempo_velocidad);
+	detenerse();
 }
 
 void Movimiento::girarDer () {
@@ -40,30 +41,4 @@ void Movimiento::detenerse () {
 void Movimiento::actualizarFactores () {
 	tiempo_velocidad = 10 * factor_distancia / (velocidad_maxima_avance);
 	tiempo_giro = 10 * factor_angulo / velocidad_maxima_giro;
-}
-
-
-int Movimiento::mover (point & posicion, point const & objetivo, point & direccion) {
-	point distancia = objetivo - posicion;
-	distancia = distancia.unitario();
-
-	bool hay_que_doblar = (direccion.x != distancia.x || distancia.x == 0) && (direccion.y != distancia.y || distancia.y == 0) ;
-
-	if (hay_que_doblar) {
-
-		if (distancia.x != 0) {
-			direccion.x = distancia.x;
-			direccion.y = 0;
-		} else {
-			direccion.x = 0;
-			direccion.y = distancia.y;
-		}
-
-	}
-
-	posicion += direccion;
-
-	// cout << "nueva posicion: " << posicion << ", despues de moverse: " << direccion << "\n\r";
-	return posicion.distancia(objetivo);
-
 }
